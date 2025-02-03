c26929e5ae7306ee525b0b21bbc57190fcac0035
minsukim9900 <mmsskksw9900@gmail.com>
2025-02-04 00:07:02 +0900

    Spring_day09_DynamicQuery_TX

---
+++ Spring_Learning_Summary/Day09_Spring_DynamicQuery_TX/src/main/java/com/ssafy/mvc/SpringDay09DynamicQueryApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.mvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDay09DynamicQueryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDay09DynamicQueryApplication.class, args);
+	}
+
+}
